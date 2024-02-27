3427d3377c05ffda1da75aff8f4f94f795ed8f05
DiogoVD <diogovdamascena@gmail.com>
2024-02-27 17:59:09 -0300

    Preparando projeto

---
+++ public/script/client.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+const usernameInput = document.querySelector("#username");
+const messageInput = document.querySelector("#message");
+const sendButton = document.querySelector("#send");
+
+sendButton.addEventListener("click", () => {
+    const username = usernameInput.value;
+    const message = messageInput.value;
+    const data = `${username}: ${message}`;
+
+    socket.emit('chat message', data);
+    messageInput.value = "";
+});
+
+// trata os dados recebidos da mensagem
+socket.on('chat message', (data) => {
+    messageInput.value += `${data}<br>`;
+});
---
+++ server.js
@@ -0,0 +1,33 @@
+// Configurando express criando servidor http e o socket.io
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// definindo rotas
+app.use("/public", express.static("public"));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+  });
+
+// inicia escuta do servidor na porta 3000
+server.listen(3000, () =>{
+    console.log('Servidor rodando na porta 3000');
+});
+
+io.on("connection", (socket) =>{
+    console.log("Usuário conectado");
+
+    socket.on('disconnect', () =>{
+        console.log("Usuário desconectado");
+    });
+
+    socket.on('chat message', (data) => {
+        // console.log(`Mensagem recebida servidor: ${data}`); //recebe os dados da mensagem.
+        // io.emit('chat message', data); //envia para todos os usuarios conectados os dados da mensagem.
+        socket.broadcast.emit('chat message', data); //envia para todos os usuarios conectados exceto o que enviou.
+    });
+});
